48c95b46bc88800067ce761bf74b64fe50f95d8a
Derian Ivan Escalante Zapata <descalante@bancodonde.com>
2024-12-27 18:15:06 -0600

    micro go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/deriannavy/microgo
+
+go 1.22.5
--- src/main.go
+++ src/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"net/http"
 )
 
@@ -8,12 +10,57 @@ type api struct {
 	addr string
 }
 
+var accounts = []Account{}
+
 func (s *api) getAccountHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Account List"))
+	w.Header().Set("Content-Type", "application/json")
+
+	err := json.NewEncoder(w).Encode(accounts)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
 
 func (s *api) createAccountHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Account Created"))
+	var payload Account
+	err := json.NewDecoder(r.Body).Decode(&payload)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	a := Account{
+		FirstName: payload.FirstName,
+		LastName:  payload.LastName,
+	}
+
+	if err = insertAccount(a); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func insertAccount(a Account) error {
+	if a.FirstName == "" {
+		return errors.New("First name is required")
+	}
+
+	if a.LastName == "" {
+		return errors.New("Last name is required")
+	}
+
+	for _, account := range accounts {
+		if account.FirstName == a.FirstName && account.LastName == a.LastName {
+			return errors.New("Account already exists")
+		}
+	}
+
+	accounts = append(accounts, a)
+	return nil
 }
 
 func main() {
@@ -30,7 +77,6 @@ func main() {
 	mux.HandleFunc("POST /accounts", api.createAccountHandler)
 
 	err := srv.ListenAndServe()
-
 	if err != nil {
 		panic(err)
 	}
