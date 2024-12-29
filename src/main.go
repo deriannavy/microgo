6a04240f00e055c49c8bea219cd36b93290b388b
Derian Escalante <deriannavy@gmail.com>
2024-12-28 19:04:53 -0600

    Automatic Update 2024-12-28_19:04:53

--- src/main.go
+++ src/main.go
@@ -1,83 +1,18 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
-	"net/http"
+	"log"
 )
 
-type api struct {
-	addr string
-}
-
-var accounts = []Account{}
-
-func (s *api) getAccountHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	err := json.NewEncoder(w).Encode(accounts)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-
-	w.WriteHeader(http.StatusOK)
-}
-
-func (s *api) createAccountHandler(w http.ResponseWriter, r *http.Request) {
-	var payload Account
-	err := json.NewDecoder(r.Body).Decode(&payload)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	a := Account{
-		FirstName: payload.FirstName,
-		LastName:  payload.LastName,
-	}
-
-	if err = insertAccount(a); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	w.WriteHeader(http.StatusCreated)
-}
-
-func insertAccount(a Account) error {
-	if a.FirstName == "" {
-		return errors.New("First name is required")
-	}
-
-	if a.LastName == "" {
-		return errors.New("Last name is required")
-	}
-
-	for _, account := range accounts {
-		if account.FirstName == a.FirstName && account.LastName == a.LastName {
-			return errors.New("Account already exists")
-		}
-	}
-
-	accounts = append(accounts, a)
-	return nil
-}
-
 func main() {
-	api := &api{addr: ":8080"}
-
-	mux := http.NewServeMux()
-
-	srv := &http.Server{
-		Addr:    api.addr,
-		Handler: mux,
+	cfg := config{
+		addr: ":8080",
 	}
 
-	mux.HandleFunc("GET /accounts", api.getAccountHandler)
-	mux.HandleFunc("POST /accounts", api.createAccountHandler)
-
-	err := srv.ListenAndServe()
-	if err != nil {
-		panic(err)
+	app := &application{
+		config: cfg,
 	}
+
+	mux := app.mount()
+	log.Fatal(app.run(mux))
 }
